e4362b50ca7f93fb525125cd6f8a4578478db433
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-06-15 23:31:46 +0900

    [unrated] Title: 코드 처리하기, Time: 0.08 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String solution(String code) {
+        
+        StringBuffer sb = new StringBuffer(code);
+        int mode = 0;
+        
+        for(int i=0; i<code.length(); i++) {
+            char code_chr = code.charAt(i);
+            if(code_chr == '1') {
+                if(mode == 0) {
+                    mode = 1;
+                } else {
+                    mode = 0;
+                };
+                continue;
+            }
+            if(mode == 0) {
+                if(i == 0 || i%2 == 0) {
+                    sb.append(code_chr);
+                }
+            } else {
+                if(i%2 != 0) {
+                    sb.append(code_chr);
+                }
+            }
+        }
+        
+        if(sb.length() == code.length()) {
+            return "EMPTY";
+        }
+            
+        String answer = sb.substring(code.length());
+        return answer;
+    }
+}
