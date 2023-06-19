ea34d82ff1f8eb88411e8215f79929279fc1464f
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-06-20 01:43:38 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.01 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -1,7 +1,15 @@
 class Solution {
     public int solution(int a, int b) {
+        
+        int num = b;
+        int digit_num = 10;
+        while(num>=10) {
+            digit_num *= 10;
+            num /= 10;
+        }
+        
         int answer = 0;
-        int a_b = Integer.parseInt(""+a+b);
+        int a_b = a*digit_num + b;
         int a_b_2 = 2*a*b;
         
         if(a_b == a_b_2) {
