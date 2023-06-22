3c17e077af0f64146126a87cb4e05cc14fe4ebad
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-06-22 16:14:50 +0900

    [unrated] Title: 달리기 경주, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,16 +2,20 @@ import java.util.HashMap;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        
         HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
         for(int i=0; i<players.length; i++) {
             map.put(players[i], i);
         }
         
         for(int i=0; i<callings.length; i++) {
             int rank = map.get(callings[i]);
+            
             String tmp = players[rank];
             players[rank] = players[rank-1];
             players[rank-1] = tmp;
+            
             map.replace(players[rank], rank);
             map.replace(players[rank-1], rank-1);
         }
