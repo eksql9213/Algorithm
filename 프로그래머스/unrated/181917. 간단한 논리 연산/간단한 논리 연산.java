7a580068511b47a2c7b510da0b71de82a0cd225a
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-02 23:42:13 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
         
-        if(!x1 && !x2 || !x3 && !x4) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return (x1 || x2) && (x3 || x4);
+        
     }
 }
