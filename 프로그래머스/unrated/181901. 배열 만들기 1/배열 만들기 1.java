7cb3fac84d784a5b665ae5a2475bf0b1ad97c5e6
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-09 13:14:28 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,19 +1,9 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int n, int k) {
-        ArrayList<Integer> multiple = new ArrayList<Integer>();
+        int[] answer = new int[n/k];
         
-        for(int i=1; i<=n; i++) {
-            if(i%k == 0) {
-                multiple.add(i);
-            }
-        }
-        
-        int[] answer = new int[multiple.size()];
-        
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = multiple.get(i);
+        for(int i=1; i<=answer.length; i++) {
+            answer[i-1] = k*i;
         }
         
         return answer;
