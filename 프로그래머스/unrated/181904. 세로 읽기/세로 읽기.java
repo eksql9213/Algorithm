14cd9280a61bbfde342b12eaa3310431d72d01e4
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-20 22:32:01 +0900

    [unrated] Title: 세로 읽기, Time: 0.11 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         StringBuffer answer = new StringBuffer();
         
-        for(int i=0; i<my_string.length(); i+=m) {
-            answer.append(my_string.substring(i, i+m).substring(c-1, c));
+        for(int i=c-1; i<my_string.length(); i+=m) {
+            answer.append(my_string.substring(i, i+1));
         }
         
         return answer.toString();
