ec3b4e41a909db205d7068a2e9a7bc337f3b8664
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-22 22:28:27 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,22 +2,15 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         
-        for(int i=0; i<answer.length; i++) {
-            int cnt = 0;
+        for(int i=0; i<my_string.length(); i++) {
+            char chr = my_string.charAt(i);
             
-            for(int j=0; j<my_string.length(); j++) {
-                char chr = (char)('A' + i);
-                
-                if(i>25) {
-                    chr = (char)(chr + 6);
-                }
-                
-                if(my_string.charAt(j) == chr) {
-                    cnt++;
-                }
+            if('A' <= chr && chr <= 'Z') {
+                answer[chr-'A'] ++;
             }
-            
-            answer[i] = cnt;
+            else {
+                answer[chr-'A'-6] ++;
+            }
         }
         
         return answer;
