22dc23a5e89a975bfcb6548e81ba95b493483c91
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-23 17:08:46 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        int length1 = b1-a1+1;
+        
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
+        int length2 = b2-a2+1;
+        
+        int[] answer = new int[length1 + length2];
+        
+        System.arraycopy(arr, a1, answer, 0, length1);
+        System.arraycopy(arr, a2, answer, length1, length2);
+        
+        return answer;
+    }
+}
