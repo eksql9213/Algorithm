00f0f8304e8758b5f390c1a5148859fdc8826e3d
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-24 23:19:53 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length-n+1];
         
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = num_list[n+i-1];
-        }
+        System.arraycopy(num_list, n-1, answer, 0, answer.length);
         
         return answer;
     }
