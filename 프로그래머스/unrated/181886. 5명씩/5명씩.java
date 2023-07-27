ef59108ff99c7230955fc0d7e9c7884569e10037
eksql9213 <126861913+eksql9213@users.noreply.github.com>
2023-07-27 23:13:36 +0900

    [unrated] Title: 5명씩, Time: 0.29 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(int)Math.ceil(names.length*1.0/5)];
+        
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = names[i*5];
+        }
+        
+        return answer;
+    }
+}
